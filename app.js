9c75a51877e9b428ffb84451b58f0b0c2fec4461
张仕博 <1279257613@qq.com>
2024-12-19 15:03:07 +0800

    配置修改

--- app.js
+++ app.js
@@ -13,18 +13,30 @@ const cors = require('cors');
 const responseHandler = require('./middleware/responseHandler');
 
 const indexRouter = require('./routes/index');
+// const { generateRandomString } = require('./utils/passwordEncrypt');
+// console.log(generateRandomString());
 
 const app = express();
 
+// 添加详细的请求日志
+app.use((req, res, next) => {
+  console.log('\n=== 请求信息 ===');
+  console.log(
+    '路由堆栈:',
+    app._router.stack.map((r) => r.route?.path || r.name)
+  );
+  console.log('请求路径:', req.path);
+  next();
+});
 // 初始化中间件
 const initMiddleware = () => {
   app.use(logger('dev'));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
   app.use(express.static(path.join(__dirname, 'public')));
-  app.use(responseHandler());
+  app.use(cookieParser());
   app.use(cors());
+  app.use(responseHandler());
 };
 
 // 初始化路由
--- utils/passwordEncrypt.js
+++ utils/passwordEncrypt.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
+const crypto = require('crypto');
 // 密码加密
 exports.passwordEncrypt = async (password) => {
   const salt = await bcrypt.genSalt(10);
@@ -10,6 +10,11 @@ exports.passwordEncrypt = async (password) => {
 // 校验密码
 exports.verifyPassword = async (password, hashedPassword) => {
   return bcrypt.compare(password, hashedPassword);
+};
+
+// 生成32位随机字符串
+exports.generateRandomString = () => {
+  return crypto.randomBytes(32).toString('hex');
 };
 
 // 生成token
